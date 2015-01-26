0d11f2131fcfe91737c9b1f96150ae7bbc9086e4
Andrew Crichton <andrew.g.crichton@gmail.com>
2015-01-25 21:11:47 -0500

    Application, activity contexts successfully injected in sample activity.

--- daggerizedtest/src/main/java/com/coatedmoose/daggerizedtest/MainActivity.java
+++ daggerizedtest/src/main/java/com/coatedmoose/daggerizedtest/MainActivity.java
@@ -1,17 +1,41 @@
 package com.coatedmoose.daggerizedtest;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.coatedmoose.daggerizedandroid.ActivityModule;
+import com.coatedmoose.daggerizedandroid.DaggerFragmentActivity;
+import com.coatedmoose.daggerizedandroid.qualifier.ForActivity;
+import com.coatedmoose.daggerizedandroid.qualifier.ForApplication;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.Module;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends DaggerFragmentActivity implements View.OnClickListener {
+
+    @Inject
+    @ForApplication
+    Context applicationContext;
+
+    @Inject
+    @ForActivity
+    Context activityContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.app_ctx_test).setOnClickListener(this);
+        findViewById(R.id.activity_ctx_test).setOnClickListener(this);
     }
 
 
@@ -35,5 +59,33 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.app_ctx_test:
+                Toast.makeText(applicationContext, R.string.toast_test_passed, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.activity_ctx_test:
+                Toast.makeText(activityContext, R.string.toast_test_passed, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                throw new IllegalStateException("Cannot handle a click there");
+        }
+    }
+
+    @Override
+    public List<Object> getModules() {
+        List<Object> modules = super.getModules();
+        modules.add(new MainActivityModule());
+        return modules;
+    }
+
+    @Module(
+            addsTo = ActivityModule.class,
+            injects = MainActivity.class
+    )
+    class MainActivityModule {
     }
 }
